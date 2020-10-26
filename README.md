aff7927692c15f3584b1d73c8b6cd4d594dc35f7
Tamur11 <tamurasar@gmail.com>
2020-10-26 03:53:10 -0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Cull-Analysis
-A computational comparison of cull vs dorans blade start with cut down.
+Computational comparison of Cull vs Doran's Blade start with cut down.
+
+Comparing HP and AD statistics from Doran's Blade and Cull we get:  
+Doran's Blade: 80 HP and 8 AD  
+Cull: 7 AD  
+
+Therefore, Cull is missing 1 AD and 80 HP in combat stats.  
+
+Converting that to pure AD using gold as a common denominator:
+400g for 150 HP from Ruby Crystal means 1 HP is roughly 2.66g  
+350g for 10 AD from Long Sword means 1 AD is roughly 35g  
+
+Therefore, 80 HP to AD is...  
+80 HP to gold is 80*2.66 = 212.8  
+212.8 gold to AD is 212.8/35 = 6.08  
+
+Therefore, Cull is roughly missing 7 AD from a combat statistics perspective.  
+
+We include absolute focus but exclude gathering storm as this primarily focuses on early levels and gathering storm only starts giving value after 10.
---
+++ calculate.py
@@ -0,0 +1,65 @@
+import csv
+
+# get user input
+cull_user = input("Please input name of cull champion: ").title()
+abs_focus = input("Absolute Focus? Boolean input: ").title() == "True"
+num_adaptive = int(input("How many adaptive runes did you take: "))
+adc = input("Please input enemy adc: ").title()
+support = input("Please input enemy support: ").title()
+
+# filter values from csv
+with open('champion statistics 10_26_2020.csv') as csv:
+    rows = [line.split(',') for line in csv.readlines()]
+    
+    for line in rows:
+        if line[0] == cull_user: cull_user_stats = line
+        if line[0] == adc: adc_stats = line
+        if line[0] == support: support_stats = line
+
+# parse results
+hp_cull_user = float(cull_user_stats[1])
+hp_scale_cull_user = float(cull_user_stats[2])
+ad_cull_user = float(cull_user_stats[9]) + (num_adaptive*5.4) + 7  # 1 adaptive rune gives 5.4 AD and 7 from Cull
+ad_scale_cull_user = float(cull_user_stats[10])
+
+# perform the calculations for enemy botlane
+for i in range(0, 2):
+    if i == 0:
+        hp_target = float(adc_stats[1]) + 80  # we assume they take shield or blade
+        hp_scale_target = float(adc_stats[2])
+        armor_target = float(adc_stats[13]) + 6  # we assume one armor rune
+        armor_scale_target = float(adc_stats[14])
+        print("\nVersus " + adc + ":")
+    else:
+        if float(support_stats[18]) > 300:  # we use champion range to determine support item
+            hp_target = float(support_stats[1]) + 10
+        else:
+            hp_target = float(support_stats[1]) + 30
+        hp_scale_target = float(support_stats[2])
+        armor_target = float(support_stats[13]) + 6  # we assume one armor rune
+        armor_scale_target = float(support_stats[14])
+        print("Versus " + support + ":")
+
+    for level in range(0, 18):  # calculate for levels 1-18
+        actual_hp_cull_user = hp_cull_user + (level*hp_scale_cull_user)
+        actual_hp_target = hp_target + (level*hp_scale_target)
+        if abs_focus:
+            actual_ad_cull_user = ad_cull_user + (level*ad_scale_cull_user) + (0.847+(0.953*(level+1)))
+        else:
+            actual_ad_cull_user = ad_cull_user + (level*ad_scale_cull_user)
+
+        percent_max_hp = (actual_hp_target-actual_hp_cull_user)/actual_hp_cull_user
+
+        bonus_damage = ((percent_max_hp-0.1)*(1/9))+0.05
+        if bonus_damage >= 0.05 and bonus_damage <= 0.15:
+            damage_being_added = actual_ad_cull_user*bonus_damage
+            print("At level " +str(level+1) + " adding " + str(damage_being_added) + " damage.")
+            seven_ad_as_damage = 7*(100/(100+armor_target+(armor_scale_target*level)))
+            print("Dorans' '7 AD' would be doing " + str(seven_ad_as_damage) + " damage.")
+            if damage_being_added > seven_ad_as_damage:
+                print("Cull is worth it.")
+            print("")
+        if bonus_damage > 0.15:
+            damage_being_added = actual_ad_cull_user*1.15
+
+
